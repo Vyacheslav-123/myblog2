0f62c4832a259b795cbf641f5cb6bbf8956efcf2
Slava <vya4eslavnor@gmail.com>
2026-01-28 20:45:48 +0300

    Added a models

--- blog/models.py
+++ blog/models.py
@@ -7,5 +7,8 @@ from django.utils import timezone
 class Post(models.Model):
     title = models.CharField(max_length=200, verbose_name="Заголовок")
     text = models.TextField(verbose_name="Текст")
-    image = models.ImageField(
-                            )
+    image = models.ImageField(upload_to='Post-image',height_field=None, width_field=None, max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+
+    def __str__(self):
+        return self.title
