eb20af5fea77756ba0533c9d7e901f83d56d2d48
Slava <vya4eslavnor@gmail.com>
2026-01-28 21:01:09 +0300

    Registred models in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Post
+# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    fields = ["title", "text", "image", "author"]
